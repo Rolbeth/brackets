f77c9771e0b3175ae39f3fce0546c64136116234
Evgeniy <106709510+Rolbeth@users.noreply.github.com>
2022-07-23 12:13:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let tpl = bracketsConfig.map(el => el.join(''));
+
+  let l = 0;
+  do {
+      l = str.length;
+      tpl.map((el) => { str = str.replace(el, '') })
+
+  } while ((str.length < l) && (str.length > 0))
+
+  if (str.length > 0) {
+      return false;
+  }
+  return true;
 }
